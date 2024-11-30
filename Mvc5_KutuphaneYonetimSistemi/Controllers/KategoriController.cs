cdc1ecd0020903b5fec51e0244b1052425ad6885
Elif Yıldırım <elifyildirim3@outlook.com>
2024-11-30 23:00:46 +0300

    Kategori sf çalışmaları

---
+++ Mvc5_KutuphaneYonetimSistemi/Controllers/KategoriController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc5_KutuphaneYonetimSistemi.Models.Entity;
+
+namespace Mvc5_KutuphaneYonetimSistemi.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            var value = db.Kategori.ToList();
+            return View(value);
+        }
+    }
+}
