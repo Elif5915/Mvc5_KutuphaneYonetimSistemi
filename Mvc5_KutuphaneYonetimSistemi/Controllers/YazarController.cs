34b0f8a42af70551c3b81e28ac13a625212469a0
Elif Yıldırım <elifyildirim3@outlook.com>
2024-12-09 00:24:30 +0300

    Yazar tablosu ile ilgili çalışma

---
+++ Mvc5_KutuphaneYonetimSistemi/Controllers/YazarController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc5_KutuphaneYonetimSistemi.Models.Entity;
+
+namespace Mvc5_KutuphaneYonetimSistemi.Controllers
+{
+    public class YazarController : Controller
+    {
+        // GET: Yazar
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            var values = db.Yazar.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult YazarEkle()
+		{
+            return View();
+		}
+
+        [HttpPost]
+        public ActionResult YazarEkle(Yazar yazar)
+		{
+            db.Yazar.Add(yazar);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+		}
+    }
+}
