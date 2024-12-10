f4d38670c600a10f879b34ff2afdf502783e7ca4
Elif Yıldırım <elifyildirim3@outlook.com>
2024-12-10 23:57:57 +0300

    Kitap sayfası için crud işlemleri

--- Mvc5_KutuphaneYonetimSistemi/Controllers/KitapController.cs
+++ Mvc5_KutuphaneYonetimSistemi/Controllers/KitapController.cs
@@ -16,5 +16,89 @@ namespace Mvc5_KutuphaneYonetimSistemi.Controllers
             var kitaplar = db.Kitap.ToList();
             return View(kitaplar);
         }
+        [HttpGet]
+        public ActionResult KitapEkle()
+		{
+            List<SelectListItem> deger = db.Kategori
+                                        .Select(i => new SelectListItem
+                                        {
+                                              Text = i.Ad,
+                                              Value = i.Id.ToString()
+
+                                        }).ToList();
+
+            ViewBag.KategoriCombo = deger;
+
+            List<SelectListItem> yazar = db.Yazar
+                                         .Select(y => new SelectListItem
+                                         {
+                                             Text = y.Ad + " " + y.Soyad,
+                                             Value = y.Id.ToString()
+
+                                         }).ToList();
+            ViewBag.YazarCombo = yazar;
+            return View();
+		}
+
+        [HttpPost]
+        public ActionResult KitapEkle(Kitap kitap)
+        {
+            var kategori = db.Kategori.Where(k => k.Id == kitap.Kategori1.Id).FirstOrDefault();
+            var yazar = db.Yazar.Where(y => y.Id == kitap.Yazar1.Id).FirstOrDefault();
+            kitap.Kategori1 = kategori;
+            kitap.Yazar1 = yazar;
+            db.Kitap.Add(kitap);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        public ActionResult KitapSil(int id)
+		{
+            var deleteBook = db.Kitap.Find(id);
+            db.Kitap.Remove(deleteBook);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+		}
+        public ActionResult KitapGetir(int id)
+        {
+            var value = db.Kitap.Find(id);
+            List<SelectListItem> deger = db.Kategori
+                                        .Select(i => new SelectListItem
+                                        {
+                                            Text = i.Ad,
+                                            Value = i.Id.ToString()
+
+                                        }).ToList();
+
+            ViewBag.KategoriCombo = deger;
+            List<SelectListItem> yazar = db.Yazar
+                                      .Select(y => new SelectListItem
+                                      {
+                                          Text = y.Ad + " " + y.Soyad,
+                                          Value = y.Id.ToString()
+
+                                      }).ToList();
+            ViewBag.YazarCombo = yazar;
+            return View("KitapGetir", value);
+
+        }
+
+        public ActionResult KitapGuncelle(Kitap kitap)
+		{
+            var value = db.Kitap.Find(kitap.Id);
+            value.Ad = kitap.Ad;
+            value.BasımYıl = kitap.BasımYıl;
+            value.YayınEvi = kitap.YayınEvi;
+            value.Sayfa = kitap.Sayfa;
+
+            var ktg = db.Kategori.Where(k => k.Id == kitap.Kategori1.Id).FirstOrDefault();
+            var yzr = db.Yazar.Where(y => y.Id == kitap.Yazar1.Id).FirstOrDefault();
+
+            value.Yazar = yzr.Id;
+            value.Kategori = ktg.Id;
+
+            db.SaveChanges();
+            return RedirectToAction("Index");
+		}
     }
 }
