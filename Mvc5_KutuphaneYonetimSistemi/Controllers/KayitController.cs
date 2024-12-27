30eddc1b3c2464ca7df27189973be7efde1cbf87
Elif Yıldırım <elifyildirim3@outlook.com>
2024-12-27 15:02:29 +0300

    Kullanıcı Paneli İçin Giriş ve Kayıt Sayfaları üzerinde yapılan çalışmalar

---
+++ Mvc5_KutuphaneYonetimSistemi/Controllers/KayitController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc5_KutuphaneYonetimSistemi.Models.Entity;
+
+namespace Mvc5_KutuphaneYonetimSistemi.Controllers
+{
+    public class KayitController : Controller
+    {
+        // GET: Kayit
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+
+        [HttpGet]
+        public ActionResult KayitOl()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult KayitOl(Uyeler uyeler)
+		{
+			if (!ModelState.IsValid)
+			{
+                return View("KayitOl");
+			}
+            db.Uyeler.Add(uyeler);
+            db.SaveChanges();
+            return View();
+		}
+    }
+}
